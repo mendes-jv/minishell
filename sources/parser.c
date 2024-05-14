027ed3f7fba90a5d70b9de0d8e6e1b0ef77e0293
mendes-jv <mendes.silva.jv@gmail.com>
2024-05-14 19:49:01 -0300

    feat: Enhance parsing functionalities in minishell
    This commit introduces new macros for defining meta-characters, operators, and quotes. In addition, it removes `equals` from e_flag enum and adds a new struct, `s_word_pattern`, to handle word-pattern matching. Lastly, it enhances the lexical analysis part with the addition of `lexer`, `get_next_word`, `word_last_char`, and `get_word_type` functions.

--- sources/parser.c
+++ sources/parser.c
@@ -1,14 +1,14 @@
 #include "../includes/minishell.h"
 
-//TODO: Set clean in fclean libft.
-
-static void	*get_next_word(char **command_line);
+static bool		lexer(char *command_line, t_list *words);
+static t_token	*get_next_word(char **command_line);
+static char		*word_last_char(char *command_line);
+static t_flag	get_word_type(char *word);
 
 bool	parser(char *command_line, t_ast *ast)
 {
-	t_list	*words;
+	t_list	words;
 
-	words = NULL;
 	if (lexer(command_line, &words))
 	{
 		// ast->word = words->content;
@@ -31,14 +31,60 @@ bool	lexer(char *command_line, t_list *words)
 	return (true);
 }
 
-static void	*get_next_word(char **command_line)
+static t_token	*get_next_word(char **command_line)
 {
-	char	*value;
-	t_flag	flag;
-	size_t	word_len;
+	char	*word;
+	char	*start;
 
-	word_len = 0;
+	while (**command_line && **command_line == ' ')
+		(*command_line)++;
+	start = *command_line;
+	*command_line = word_last_char(*command_line);
+	if (!**command_line)
+		(*command_line)--;
+	word = ft_substr(start, 0, *command_line - start);
+	return (&(t_token) {word, get_word_type(word)});
+}
 
+static char	*word_last_char(char *command_line)
+{
+	char quote_type;
+	if (ft_strchr(METACHARS, *command_line))
+	{
+		if (ft_strchr(OPERATORS, *command_line) && *command_line == *(command_line + 1))
+			return (command_line + 1);
+		if (*command_line != '&')
+			return (command_line);
+	}
+	if (ft_strchr(QUOTES, *command_line))
+	{
+		quote_type = *command_line;
+		command_line++;
+		while (*command_line && *command_line != quote_type)
+			command_line++;
+		return (command_line);
+	}
+	while (*command_line && *command_line != ' ' && !ft_strchr(METACHARS, *command_line))
+			command_line++;
+	return (command_line);
+}
 
-	return ((void *) {value, flag});
+static t_flag	get_word_type(char *word)
+{
+	t_word_pattern *word_patterns;
+
+	word_patterns = (t_word_pattern[11]){
+		{"|", PIPE}, {">", GREATER}, {"<", LESSER},
+		{">>", DOUBLE_GREATER}, {"<<", DOUBLE_LESSER}, {"||", DOUBLE_PIPE},
+		{"&&", DOUBLE_AND}, {"(", LEFT_PARENTHESIS}, {")", RIGHT_PARENTHESIS},
+		{"*", WILDCARD}, {NULL, WORD}
+	};
+
+	while (word_patterns->pattern)
+	{
+		if (!ft_strncmp(word, word_patterns->pattern, ft_strlen(word_patterns->pattern)))
+			return (word_patterns->flag);
+		word_patterns++;
+	}
+	return (WORD);
 }
