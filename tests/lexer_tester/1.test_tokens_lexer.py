2fb5aabcce1e923d72bc0c3f18be6cf5d36d27ca
Pedro Melo <pedromelocf@yahoo.com>
2024-05-16 21:42:48 -0300

    Removing unecessary tests for this time and fixing tests for a hardcoded solution

--- tests/lexer_tester/1.test_tokens_lexer.py
+++ tests/lexer_tester/1.test_tokens_lexer.py
@@ -9,12 +9,22 @@ from time import sleep
 # de quoting.
 
 def main():
+	index = 0
 	test_scenarios = [
+	"   'gre'pattern file.txt",
+	"awk '{print $1}'file.txt",
+	"   maybe the \"last\"$ time 'last'$time",
+	"testing quotes: '   hello!!  spaces need to     stay    '",
+	"\"\" oi",
+	"'' oi",
+	"oi mundo",
+	"echo \"hello\"   world",
 	"echo $?",
 	"'echo    $minishell   ' ",
 	"Hello, world!",
+	"Hey '$world' \"   $env\"  ",
 	"echo  $minihell",
-	"    testing 	 trailling    spaces  and tab    inside    string		",
+	"    t 	 trailling    spaces  and tab    inside    string		"
 	"testing type long long max: +9223372036854775807    ",
 	"testing type long long min: -9223372036854775807    ",
 	"testing quotes: '   hello!!  spaces need to     stay    '",
@@ -22,7 +32,6 @@ def main():
 	"	quotes one more    tim\"E\", and what about ' \"doubles          inside          \" ' singles? ",
 	"	i hate quotes \"  but '  	lets    ' '    deal    ' \" with them \" ls :\"",
 	"    last but not \" least '' or 	maybe   ''\"   	",
-	"   maybe the \"last\"$ time 'last'$time",
 	"  ls -la |    echo Oi  ",
 	"  ls  ",
 	"   pw0-d-- 81 ",
@@ -31,7 +40,6 @@ def main():
 	"!\-- -2-3 --30-0=++33 *",
 	".lscf ffd   ff  f",
 	"    echo     Hello    ; ls  ",
-	"\\",
 	"ls ;",
 	" $val ",
 	"$valgrind",
@@ -43,32 +51,90 @@ def main():
 	"  [] <;  &&    ",
 	"    65  worl d || >|   * test \\",
 	"   	$ $ $ $ $   > < >> || <<     ''   ?   ? ** 4434   -00- _34331d 2342234  "" -ls-- || red | >> !  ",
-	"   grep 'pattern' file.txt",
 	"sed 's/old/file/file.txt'",
-	"awk '{print $1}'file.txt",
 	"  chmod  +x outfile   ",
 	"touch infile",
 	"cut -d ':' -f 1 /usr/local",
-	"(grep 'error'     logfile || (cat error.log    |    sed 's/error/warning/g' >    error_warnings.log))   &&   (echo 'No errors found!'   >    status.log)   ||   echo   'Errors detected!'"
+	"(grep 'error'     logfile || (cat error.log    |    sed 's/error/warning/g' >    error_warnings.log))   &&   (echo 'No errors found!'   >    status.log)   ||   echo   'Errors detected!'",
+	"cat banana && cat abacaxi",
+	"cat banana &&cat abacaxi",
+	"cat banana&&cat abacaxi",
+	"cat banana&& cat abacaxi",
+	"echo test || echo test",
+	"echo test ||echo test",
+	"echo test||echo test",
+	"echo test|| echo test",
+	"cat LICENSE | grep Copy",
+	"cat LICENSE |grep Copy",
+	"cat LICENSE| grep Copy",
+	"cat LICENSE|grep Copy"
 	]
 
 	for script_content in test_scenarios:
 		bash = script_content
-		bash_args_output = remove_extra_spaces(bash)
+		tokens = tokenizer(index)
+		index += 1
 		minishell = run_minishell_script(script_content)
 		minishell_output = str(minishell.stdout)
-		val = subprocess.run('cat val.txt | wc -l', shell=True, capture_output=True, text=True)
-		test_outputs(bash_args_output, minishell_output, val, bash)
+		val = subprocess.run('cat tests/temp_files/val.txt | wc -l', shell=True, capture_output=True, text=True)
+		test_outputs(tokens, minishell_output, val, bash, index)
 		sleep(0.6)
+	subprocess.run('rm tests/temp_files/val.txt', shell=True)
+	return
+
+def tokenizer(index):
+	i = 0
+	with open('tests/temp_files/lexer_model.txt', 'r') as t:
+		for l, line in enumerate(t):
+			if l == index:
+				temp = line.replace("\n", "", 1)
+				return temp
+
+def run_minishell_script(script_content):
+	with tempfile.NamedTemporaryFile('w', delete=False) as script_file:
+		script_file.write(script_content)
+	minishell_command = f'./minishell {script_file.name}'
+	result = subprocess.run(minishell_command, input=script_content, shell=True, capture_output=True, text=True)
+	with open('tests/temp_files/val.txt', 'w') as v:
+		valgrind_command = ['valgrind', '-q', '--leak-check=full', '--show-leak-kinds=all', './minishell']
+		valgrind_command.extend([script_file.name])
+		subprocess.run(valgrind_command, input=script_content, text=True, stderr=v, stdout=subprocess.DEVNULL)
+	os.unlink(script_file.name)
+	return result
+
+def test_outputs(tokens, minishell_output, val, bash, index):
+	print("\n")
+	print_yellow("----------Test ")
+	print(index, end=' ')
+	print_yellow("----------")
+	print("\n")
+	print_purple("INPUT: " + "[" + bash + "]" + "\n")
+	if tokens == minishell_output:
+		print_green("TOKENS CHECK [OK]")
+		print("ACTUAL TOKENS: ", end ='')
+		print(tokens)
+	else:
+		print_red("TOKENS CHECK [KO]")
+		print("\nEXPECTED TOKENS: ", end='')
+		print(tokens, end='')
+		print("\nACTUAL TOKENS: ", end ='')
+		print(minishell_output)
+	if val.stdout.strip() == '0':
+		print_green("\nMEM CHECK [MOK]")
+	else:
+		print_red("\nMEM CHECK [MKO]")
+		print("\nVALGRIND ERROR:")
+		subprocess.run('cat tests/temp_files/val.txt', shell=True)
 
 class Color:
 	GREEN = '\033[92m'
 	RED = '\033[91m'
 	PURPLE = '\033[95m'
+	YELLOW = '\033[93m'
 	RESET = '\033[0m'
 
 def print_red(text):
-	print(Color.RED + text + Color.RESET)
+	print(Color.RED + text + Color.RESET, end="")
 
 def print_green(text):
 	print(Color.GREEN + text + Color.RESET)
@@ -76,88 +142,8 @@ def print_green(text):
 def print_purple(text):
 	print(Color.PURPLE + text + Color.RESET)
 
-def	get_lenght_without_end_spaces_or_tabs(bash):
-	i = 0
-	lenght = len(bash)
-	while (i < lenght):
-		if bash[lenght - 1] == " " or bash[lenght - 1] == "	":
-			lenght -= 1
-		else:
-			break
-	return (lenght)
-
-def remove_extra_spaces(bash):
-	space = 1
-	i = 0
-	newstring = ""
-	lenght = get_lenght_without_end_spaces_or_tabs(bash)
-	while i < lenght:
-		y = 0
-		z = 0
-		if bash[i] == "\"":
-			x = i + 1
-			while x < lenght:
-				if bash[x] == "\"":
-					space = 0
-					break
-				else:
-					y += 1
-					x += 1
-			while z <= y + 1:
-				newstring += bash[i]
-				z += 1
-				i += 1
-			space = 0
-			i -= 1
-		elif bash[i] == "'":
-			x = i + 1
-			while x < lenght:
-				if bash[x] == "'":
-					space = 0
-					break
-				else:
-					y += 1
-					x += 1
-			while z <= y + 1:
-				newstring += bash[i]
-				z += 1
-				i += 1
-			space = 0
-			i -= 1
-		elif bash[i] == " " or bash[i] == "\t":
-			if space == 0:
-				newstring += " "
-				space = 1
-		else:
-			space = 0
-			newstring += bash[i]
-		i += 1
-	return(newstring)
-
-def run_minishell_script(script_content):
-	with tempfile.NamedTemporaryFile('w', delete=False) as script_file:
-		script_file.write(script_content)
-	minishell_command = f'./test_input {script_file.name}'
-	result = subprocess.run(minishell_command, input=script_content, shell=True, capture_output=True, text=True)
-	with open('val.txt', 'w') as v:
-		valgrind_command = ['valgrind', '-q', '--leak-check=full', '--show-leak-kinds=all', './test_input']
-		valgrind_command.extend([script_file.name])
-		subprocess.run(valgrind_command, input=script_content, text=True, stderr=v, stdout=subprocess.DEVNULL)
-	os.unlink(script_file.name)
-	return result
-
-def test_outputs(bash_args_output, minishell_output, val, bash):
-	print_purple("TESTING INPUT: " + "[" + bash + "]")
-	if bash_args_output == minishell_output:
-		print_green("INPUT CHECK [OK]")
-	else:
-		print_red("INPUT CHECK [KO]")
-		print_red("EXPECTED: " + "[" + bash_args_output + "]")
-		print_red("OUTPUT: " + "[" + minishell_output + "]")
-	if val.stdout.strip() == '0':
-		print_green("MEM CHECK [MOK]" + "\n")
-	else:
-		print_red("MEM CHECK [MKO]" + "\n")
+def print_yellow(text):
+	print(Color.YELLOW + text + Color.RESET, end='')
 
 if __name__ == "__main__":
 	main()
