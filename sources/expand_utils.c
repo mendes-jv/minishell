c178e0c1385bc2187a5aac2b379502b86b46ef84
mendes-jv <mendes.silva.jv@gmail.com>
2024-07-28 21:13:53 -0300

    feat: add ft_strfjoin function and improve string handling
    Introduced ft_strfjoin function in the libft library for string concatenation and memory management. Improved string handling in expand_utils.c by refactoring string processing functions and adding utilities for handling quotes and dollar signs.

--- sources/expand_utils.c
+++ sources/expand_utils.c
@@ -1,5 +1,9 @@
 # include "../includes/minishell.h"
 
+char	*clean_string(char *cmd);
+char	*skip_single_quotes(char *cmd, size_t *index);
+char	*skip_double_quotes(char *cmd, size_t *index);
+char	*skip_dollar_sign(char *cmd, size_t *index);
 char	**expand_string(char *cmd);
 
 void	expand_heredoc(char *doc_line, pid_t pipe_fd)
@@ -32,19 +36,88 @@ char	**expand_string(char *cmd)
 
 char	*clean_string(char *cmd)
 {
-	char	*cleaned_cmd;
+	char	*clean;
+	size_t	index;
 
-	cleaned_cmd = NULL;
-	while (*cmd)
+	clean = ft_strdup("");
+	index = 0;
+	while (cmd[index])
 	{
-		if (*cmd == '\'')
-			cleaned_cmd = skip_single_quotes(cmd);
-		else if (*cmd == '\"')
-			cleaned_cmd = skip_double_quotes(cmd);
-		else if (*cmd == '$')
-			cleaned_cmd = skip_dollar_sign(cmd);
+		if (cmd[index] == '\'')
+			clean = skip_single_quotes(cmd, &index);
+		else if (cmd[index] == '\"')
+			clean = ft_strfjoin(cmd, skip_double_quotes(cmd, &index));
+		else if (cmd[index] == '$')
+			clean = ft_strfjoin(cmd, skip_dollar_sign(cmd, &index));
 		else
-			(void) cleaned_cmd; //TODO: join normal word
+			sdfgsdfgdf //TODO: join normal word
 	}
-	return (cleaned_cmd);
+	return (clean);
+}
+
+char	*skip_single_quotes(char *cmd, size_t *index)
+{
+	size_t	start;
+
+	start = *index;
+	(*index)++;
+	while (cmd[*index] != '\'')
+		(*index)++;
+	(*index)++;
+	return (ft_strfjoin(cmd, ft_substr(cmd, start, *index - start)));
+}
+
+char	*skip_double_quotes(char *cmd, size_t *index)
+{
+	char	*clean;
+
+	clean = ft_strdup("\"");
+	(*index)++;
+	while (cmd[*index] != '\"')
+	{
+		if (cmd[*index] == '$')
+			clean = ft_strfjoin(clean, skip_dollar_sign(cmd, index));
+		else
+			clean = ft_strfjoin(clean, double_quotes_str(cmd, index));
+	}
+	(*index)++;
+	return (ft_strfjoin(clean, ft_strdup("\"")));
+}
+
+char	*skip_dollar_sign(char *cmd, size_t *index)
+{
+	size_t	start;
+	char	*substring;
+	char	*env_var;
+
+	(*index)++;
+	if (ft_isdigit(cmd[*index]) || cmd[*index] == '@')
+	{
+		(*index)++;
+		return (ft_strdup(""));
+	}
+	else if (cmd[*index] == '?') {
+		(*index)++;
+		return (ft_itoa(0)); //TODO: return exit_status
+	}
+	else if (!ft_isalnum(cmd[*index]) && cmd[*index] != '_')
+		return (ft_strdup("$"));
+	start = *index;
+	while (ft_isalnum(cmd[*index]) || cmd[*index] == '_')
+		(*index)++;
+	substring = ft_substr(cmd, start, *index - start);
+	env_var = get_env_var(env, substring); //TODO: check how to get env_list to compare here
+	if (!env_var)
+		return (free(substring), ft_strdup(""));
+	return (free(substring), ft_strdup(env_var));
+}
+
+char	*double_quotes_str(char *cmd, size_t *index)
+{
+	size_t	start;
+
+	start = *index;
+	while (cmd[*index] != '\"')
+		(*index)++;
+	return (ft_substr(cmd, start, *index - start));
 }
